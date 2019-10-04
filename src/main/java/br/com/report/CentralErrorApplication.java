b6f2a81fa18feb37e5a04edfebf507373793cc34
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-03 21:28:02 -0300

    feat: created class Main CentralErrorApplication

---
+++ src/main/java/br/com/report/CentralErrorApplication.java
@@ -0,0 +1,13 @@
+package br.com.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralErrorApplication.class, args);
+	}
+
+}
