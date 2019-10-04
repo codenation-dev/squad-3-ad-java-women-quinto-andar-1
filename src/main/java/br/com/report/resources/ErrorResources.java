6a57c7acc30c596c6f3d314875f425b33bf45d0f
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-03 21:27:42 -0300

    feat: created class ErrorResources

---
+++ src/main/java/br/com/report/resources/ErrorResources.java
@@ -0,0 +1,9 @@
+package br.com.report.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ErrorResources {
+}
