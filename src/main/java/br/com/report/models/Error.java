1fab1d1155fd1e69fd2e4320772ecf53888d7bc5
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-08 15:21:34 -0300

    feat: created class Error

--- src/main/java/br/com/report/models/Error.java
+++ src/main/java/br/com/report/models/Error.java
@@ -1,4 +1,51 @@
 package br.com.report.models;
 
-public class Error {
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "TB_ERRO")
+public class Error implements Serializable {
+
+    private static final long serialVersionId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LogId")
+    private long id;
+/*
+    @Column(name = "CollectedBy")
+    private int collectedBy;
+*/
+    @Column(name="Level", nullable = false)
+    @Size(min = 1, max = 20)
+    private String level;
+
+    @Column(name="Origin", nullable = false)
+    @Size(min = 1, max = 50)
+    private String origin;
+
+    @Column(name="Description", nullable = false)
+    @Size(min = 1, max = 128)
+    private String description;
+
+    @Column(name="Details", nullable = false)
+    @Size(min = 1, max = 300)
+    private String details;
+
+    private Timestamp dataLogged;
+
+    @Column(name="Status", nullable = false)
+    @Size(min = 1, max = 10)
+    private String status;
+/*
+    @Column(name = "LastModifiedBy")
+    private int lastModifiedBy;
+*/
+    @Column(name="Environment", nullable = false)
+    @Size(min = 1, max = 50)
+    private String environment;
+
 }
