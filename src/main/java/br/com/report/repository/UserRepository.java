551249b6c61081ce5970a79a8cf4d51c599d898d
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-08 15:54:25 -0300

    fix: rename class UserRepository

--- src/main/java/br/com/report/repository/UserRepository.java
+++ src/main/java/br/com/report/repository/UserRepository.java
@@ -3,7 +3,7 @@ package br.com.report.repository;
 import br.com.report.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ErrorCenterRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findById(long id);
 }
