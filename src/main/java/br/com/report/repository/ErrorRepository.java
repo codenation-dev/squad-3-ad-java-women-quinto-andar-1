6facde0288609944ad61b316757955cf7790aaf8
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-08 20:44:50 -0300

    fix: add import

--- src/main/java/br/com/report/repository/ErrorRepository.java
+++ src/main/java/br/com/report/repository/ErrorRepository.java
@@ -1,5 +1,6 @@
 package br.com.report.repository;
 
+import br.com.report.models.Error;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ErrorRepository extends JpaRepository<Error, Long> {
