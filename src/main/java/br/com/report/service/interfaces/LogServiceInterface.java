63e9abf470bbb331ea712f37e1f605123f01d15c
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-22 23:23:07 -0300

    fix: organized and rename code

---
+++ src/main/java/br/com/report/service/interfaces/LogServiceInterface.java
@@ -0,0 +1,30 @@
+package br.com.report.service.interfaces;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import br.com.report.model.Log;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LogServiceInterface {
+
+    Log toSave(Log log);
+
+    Optional<Log> findById(Long id);
+
+
+    void changeStatus(Log log);
+
+    List<Log> findAll();
+
+    List<Log> findLogByEnvironment(String environment);
+
+    List<Log> findLogByEnvironmentAndOrderBy(String environment, String orderBy);
+
+    List<Log> findLogByEnvironmentAndSearchBy(String environment, String searchBy);
+
+    List<Log> findLogByEnvironmentAndOrderByAndSearchBy(String environment, String orderBy, String searchBy);
+
+}
