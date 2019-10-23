268ad1f5a02cd3d6fb0463d8ae3c4ae66bc0ce94
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-23 15:27:13 -0300

    fix: organized code

--- src/main/java/br/com/report/report/controller/LogController.java
+++ src/main/java/br/com/report/report/controller/LogController.java
@@ -1,7 +1,8 @@
-package br.com.report.controller;
+package br.com.report.report.controller;
 
-import br.com.report.model.Log;
-import br.com.report.service.LogService;
+import br.com.report.report.model.Log;
+
+import br.com.report.report.service.LogServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,46 +14,46 @@ import java.util.Optional;
 public class LogController {
 
     @Autowired
-    private LogService logService;
+    private LogServiceImp logServiceImp;
 
 
     @PostMapping("/log")
     public Log toSave(@RequestBody Log log){
-        return logService.toSave(log);
+        return logServiceImp.toSave(log);
     }
 
     @GetMapping("/log/{id}")
     public Optional<Log> findById(@PathVariable(value = "id")  Long id){
-        return logService.findById(id);
+        return logServiceImp.findById(id);
     }
 
     @PutMapping("/log")
     public void changeStatus(@RequestBody Log log){
-        logService.changeStatus(log);
+        logServiceImp.changeStatus(log);
     }
 
     @GetMapping("/log")
     public List<Log> findAll() {
-        return logService.findAll();
+        return logServiceImp.findAll();
     }
 
     @GetMapping("/log/{environment}")
     public List<Log> findLogByEnvironment(@PathVariable(value = "environment") String environment) {
-        return logService.findLogByEnvironment(environment);
+        return logServiceImp.findLogByEnvironment(environment);
     }
 
     @GetMapping("/log/{environment}/{orderBy}")
     public List<Log> findLogByEnvironmentAndOrderBy(@PathVariable(value = "environment") String environment, @PathVariable(value = "orderBy") String orderBy){
-        return logService.findLogByEnvironmentAndOrderBy(environment,orderBy);
+        return logServiceImp.findLogByEnvironmentAndOrderBy(environment,orderBy);
     }
     @GetMapping("/log/{environment}/{searchBy}")
     public List<Log> findLogByEnvironmentAndSearchBy(@PathVariable(value = "environment")String environment, @PathVariable(value = "searchBy")String searchBy){
-        return logService.findLogByEnvironmentAndSearchBy(environment, searchBy);
+        return logServiceImp.findLogByEnvironmentAndSearchBy(environment, searchBy);
     }
 
     @GetMapping("/log/{environment}/{orderBy}/{searchBy}")
     public List<Log> findLogByEnvironmentAndOrderByAndSearchBy(@PathVariable(value = "environment") String environment,@PathVariable(value = "orderBy") String orderBy,@PathVariable(value="searchBy") String searchBy) {
-        return logService.findLogByEnvironmentAndOrderByAndSearchBy(environment, orderBy, searchBy);
+        return logServiceImp.findLogByEnvironmentAndOrderByAndSearchBy(environment, orderBy, searchBy);
     }
 
 
