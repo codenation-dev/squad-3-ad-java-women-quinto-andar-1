ab63c6d1199ea0e48b4a58d22b0c72414d821fc4
IsMello <is-mello@hotmail.com>
2019-10-30 19:42:51 -0300

    fix: tratando erros no Token controller

--- src/main/java/br/com/report/controller/TokenController.java
+++ src/main/java/br/com/report/controller/TokenController.java
@@ -4,6 +4,8 @@ import br.com.report.entity.Token;
 import br.com.report.service.impl.TokenService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,14 +17,22 @@ public class TokenController {
 
     @PostMapping("/token")
     @ApiOperation(value = "Adds Tokens to the database.")
-    public Token addToken(@RequestBody Token token){
-        return tokenServiceImpl.addToken(token);
+    public ResponseEntity<Token> addToken(@RequestBody Token token){
+       try{
+        return new ResponseEntity<>(tokenServiceImpl.addToken(token), HttpStatus.CREATED);
+       }catch (Exception e){
+           throw e;
+       }
     }
 
     @GetMapping("/token/{id}")
     @ApiOperation(value = "Finds a Token through its id.")
-    public Token findById(@PathVariable(value = "id") long id){
-        return tokenServiceImpl.findById(id);
+    public ResponseEntity<Token> findById(@PathVariable(value = "id") long id){
+       try{
+        return new ResponseEntity<>(tokenServiceImpl.findById(id), HttpStatus.OK);
+       } catch (Exception e) {
+         throw e;
+       }
     }
 
 }
