9529fd4d6d63a352e66f2f4db463ee6bbddd5d1a
tainajmedeiros <tainajmedeiros@gmail.com>
2019-10-30 14:48:09 -0300

    feat: created class about payload

---
+++ src/main/java/br/com/report/payload/LoginRequest.java
@@ -0,0 +1,27 @@
+package br.com.report.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
