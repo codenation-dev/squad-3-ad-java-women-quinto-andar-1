228f706f57590b856804c47187505a02a1be0ec3
Jacqueline <jsmk@unifei.edu.br>
2019-10-31 20:45:20 -0300

    fix: login and logout

--- ErrorCenter-Angular/src/app/services/login.service.ts
+++ ErrorCenter-Angular/src/app/services/login.service.ts
@@ -12,16 +12,31 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(username, password){
-    return this.http.post<any>( this.url + '/authenticate', {username,password}).pipe(
+    let body = {
+      "login": username,
+      "password": password
+    }
+    return this.http.post<any>( this.url + '/api/auth/login', body).pipe(
       map(
         userData => {
+          console.log("userData");
+          console.log(userData);
+
          sessionStorage.setItem('username',username);
-         let tokenStr= 'Bearer '+userData.token;
+         let tokenStr= 'Bearer ' + userData.accessToken;
          sessionStorage.setItem('token', tokenStr);
          return userData;
         }
       )
  
      );
+     
+    //return this.http.post<any>(this.url + '/api/auth/login', body);
   }
+
+  logOut() {
+    sessionStorage.removeItem('username');
+    sessionStorage.removeItem('token');
+  }
+
 }
