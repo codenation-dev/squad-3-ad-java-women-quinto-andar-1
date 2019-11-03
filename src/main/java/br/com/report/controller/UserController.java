329756494ddf55fb0e61c60d3ddc847894e93b00
tainajmedeiros <tainajmedeiros@gmail.com>
2019-11-03 20:58:44 -0300

    fix:add information for using swagger

--- src/main/java/br/com/report/controller/UserController.java
+++ src/main/java/br/com/report/controller/UserController.java
@@ -17,21 +17,25 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "Add a new user in database")
     @PostMapping("/user")
     public User addUser(@RequestBody User user){
         return userService.addUser(user);
     }
 
+    @ApiOperation(value = "Finds a user by its id")
     @GetMapping("/user/{id}")
     public Optional<User> findById(@PathVariable(value = "id") long id){
         return userService.findById(id);
     }
 
+    @ApiOperation(value = "Return a list with all the users")
     @GetMapping("/users")
     public List<User> findAll(){
         return userService.findAll();
     }
 
+    @ApiOperation(value = "Modify user on / off status")
     @PutMapping("/user")
     public void changeStatus(@RequestBody User user){
          userService.changeStatus(user);
