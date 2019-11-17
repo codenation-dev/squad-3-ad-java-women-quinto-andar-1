92b3a32a06207aab6abfb76aa142bd440ae60d0d
tainajmedeiros <tainajmedeiros@gmail.com>
2019-11-17 18:35:33 -0300

    fix: add variable date and constructor empty

--- src/main/java/br/com/report/entity/Log.java
+++ src/main/java/br/com/report/entity/Log.java
@@ -2,11 +2,14 @@ package br.com.report.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.h2.api.DatabaseEventListener;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.time.Instant;
 
@@ -38,9 +41,9 @@ public class Log implements Serializable {
     @Size(max = 300)
     private String details;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private Instant dataLogged;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Date dataLogged;
 
     @Column(name="status", nullable = false)
     @Size(max = 10)
@@ -50,16 +53,24 @@ public class Log implements Serializable {
     @Size(max = 50)
     private String environment;
 
+    @Column(name = "event",nullable = false)
+    private int event;
+
     @OneToOne
     private User user;
 
-    public Log(@Size(max = 20) String level, @Size(max = 50) String origin, @Size(max = 128) String description, @Size(max = 300) String details, @Size(max = 10) String status, @Size(max = 50) String environment, User user) {
+    public Log(){
+
+    }
+    public Log(@Size(max = 20) String level, @Size(max = 50) String origin, @Size(max = 128) String description, @Size(max = 300) String details, @Size(max = 10) String status, @Size(max = 50) String environment, int event, User user) {
         this.level = level;
         this.origin = origin;
         this.description = description;
         this.details = details;
         this.status = status;
         this.environment = environment;
+        this.event = event;
         this.user = user;
     }
+
 }
