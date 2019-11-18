757e6149adb29ca2d9a75adca8cb5ff64ef315f9
Jacqueline <jsmk@unifei.edu.br>
2019-11-18 15:45:53 -0300

    fix: changing url

--- ErrorCenter-Angular/src/app/services/variable-environment.service.ts
+++ ErrorCenter-Angular/src/app/services/variable-environment.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class VariableEnvironmentService {
 
-  //public url = 'https://central-de-erros.herokuapp.com';
-  public url = 'http://localhost:8080';
+  public url = 'https://central-de-erros.herokuapp.com';
+  //public url = 'http://localhost:8080';
 
 
   constructor() { }
