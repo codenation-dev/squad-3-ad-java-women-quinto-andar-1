433093a2d7beb1a6d93b8e6e3e7f800474b62703
Jacqueline <jsmk@unifei.edu.br>
2019-11-18 19:56:00 -0300

    fix: added user and JwtAuthentication to responseDTO

--- src/main/java/br/com/report/dto/ResponseDTO.java
+++ src/main/java/br/com/report/dto/ResponseDTO.java
@@ -1,4 +1,26 @@
 package br.com.report.dto;
 
+import br.com.report.entity.User;
+import br.com.report.payload.JwtAuthenticationResponse;
+
 public class ResponseDTO {
+
+    public User user;
+    public JwtAuthenticationResponse jwtAuthenticationResponse;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public JwtAuthenticationResponse getJwtAuthenticationResponse() {
+        return jwtAuthenticationResponse;
+    }
+
+    public void setJwtAuthenticationResponse(JwtAuthenticationResponse jwtAuthenticationResponse) {
+        this.jwtAuthenticationResponse = jwtAuthenticationResponse;
+    }
 }
