92f7fd19f5403d5a380e6afbd1546e6c41e8c9cd
tainajmedeiros <tainajmedeiros@gmail.com>
2019-11-19 00:17:49 -0300

    fix: remove methods not used

--- src/main/java/br/com/report/payload/JwtAuthenticationResponse.java
+++ src/main/java/br/com/report/payload/JwtAuthenticationResponse.java
@@ -12,16 +12,9 @@ public class JwtAuthenticationResponse {
         return accessToken;
     }
 
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
     public String getTokenType() {
         return tokenType;
     }
 
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
 }
 
